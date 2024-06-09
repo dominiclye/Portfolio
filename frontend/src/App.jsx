a32e76aa7758a7be33db22bcec840af9626c6eb9
dominiclye <dominiclye91@gmail.com>
2024-06-09 23:56:35 +1000

    hero section work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,7 @@ function App() {
 
   return (
     <>
-    <div className="page-content">
       <LandingPage />
-    </div>
     </>
   )
 }
--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -2,33 +2,34 @@ import React from 'react'
 import './LandingPage.css'
 import Navbar from '../../components/NavBar/Navbar'
 import heroPhoto from '../../assets/hero-photo.jpg'
+import githubIcon from '../../assets/github.svg'
+import instagramIcon from '../../assets/instagram.svg'
+import linkedinIcon from '../../assets/linkedin.svg'
+
+
 
 const LandingPage = () => {
   return (
     <div className="bg-grid">
         <Navbar />
         <div className="hero">
-            <div className="portrait-container">
-                <img src={heroPhoto} alt="portrait" className='portrait' />
-                <p className="caption">📍 Hakuba, Japan</p>
-            </div>
+                <div className="portrait-container">
+                    <img src={heroPhoto} alt="portrait" className='portrait' />
+                    <p className="caption">📍 Hakuba, Japan</p>
+                </div>
 
-            <div className="text-container">
-                <h1 className="hero-title">Hello, I'm <span className='hero-name'>Dominic</span></h1>
-                <p className="hero-text">A first-year student at The University of Melbourne, completing a Bachelor of Science with hopes of majoring in Data Science</p>
-            </div>
-            
+                <div className="text-container">
+                    <h1 className="hero-title">Hello, I'm <span className='hero-name'>Dominic</span></h1>
+                    <p className="hero-text">A first-year student at The University of Melbourne, completing a Bachelor of Science with hopes of majoring in Data Science</p>
+
+
+                </div>
+
         </div>
-
-        <div className="skills">
-            <h1>Skills</h1>
-            <ul className="skill-icons">
-                <li className='skill-icon'>Python</li>
-                <li className='skill-icon'>JavaScript</li>
-                <li className='skill-icon'>Golang</li>
-                <li className='skill-icon'>SQL</li>
-                <li className='skill-icon'>Docker</li>
-            </ul>
+        <div className="hero-links">
+                        <a href="https://github.com/dominiclye" target="_blank" rel="noreferrer" className="link-wrapper"><img src={githubIcon} alt="#" className="hero-link" /></a>
+                        <a href="https://github.com/dominiclye" target="_blank" rel="noreferrer" className="link-wrapper"><img src={instagramIcon} alt="#" className="hero-link" /></a>
+                        <a href="https://github.com/dominiclye" target="_blank" rel="noreferrer" className="link-wrapper"><img src={linkedinIcon} alt="#" className="hero-link" /></a>
         </div>
     </div>
 
